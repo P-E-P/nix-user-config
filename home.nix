1961f67028144554c14280e23cc9e30abf436359
Pierre-Emmanuel Patry <pierre-emmanuel.patry@epita.fr>
2021-10-10 13:41:01 +0200

    refacto: Add some package, battery module and update discord configuration.

--- home.nix
+++ home.nix
@@ -2,11 +2,23 @@
 let
   tin = pkgs.callPackage ./tin.nix {};
   unstable = import <nixos-unstable> { config.allowUnfree = true; };
+  discordUpdated = pkgs.discord.override rec {
+
+    version = "0.0.16";
+    src = builtins.fetchurl {
+      url = "https://dl.discordapp.net/apps/linux/${version}/discord-${version}.tar.gz";
+      sha256 = "1s9qym58cjm8m8kg3zywvwai2i3adiq6sdayygk2zv72ry74ldai";
+    };
+  };
 in
   {
   #  xsession.enable = true;
   #  xsession.windowManager.command = "…";
   xsession.windowManager.i3 = import ./i3.nix { inherit pkgs lib; };
+
+  imports = [
+        ./battery.nix
+          ];
 
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
@@ -14,10 +26,12 @@ in
     username = "und";
     homeDirectory = "/home/und";
     packages = with pkgs; [
-      unstable.idea.clion ghidra-bin vlc obs-studio nethogs tin woof qemu
-      flameshot
+      unstable.idea.clion unstable.idea.pycharm-professional unstable.idea.idea-ultimate ghidra-bin vlc obs-studio nethogs tin woof qemu
+      flameshot wireguard boost discordUpdated unstable.idea.pycharm-professional minecraft
+      unstable.adoptopenjdk-openj9-bin-16 maven libnotify sshfs android-studio texlive.combined.scheme-full jetbrains.rider dotnet-sdk_3
     ];
   };
+
 
   programs = {
     home-manager.enable = true;
