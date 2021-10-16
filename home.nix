bfe158fd1f87cf15a559831be500896b3a43a180
Pierre-Emmanuel Patry <pierre-emmanuel.patry@epita.fr>
2021-10-16 20:36:33 +0200

    feat (vim): Add vim configuration.
    Complete the migration of the vim config from a system wide environment
    to home-manager.

--- home.nix
+++ home.nix
@@ -36,6 +36,28 @@ in
     home-manager.enable = true;
     command-not-found.enable = true;
 
+    vim = {
+      enable = true;
+      plugins = with pkgs.vimPlugins; [
+        vim-airline
+        rust-vim
+        vim-nix
+        vim-gutentags
+        jellybeans-vim
+      ];
+      settings = {
+        relativenumber = true;
+        number = true;
+        expandtab = true;
+        ignorecase = true;
+        shiftwidth = 4;
+        # The length of a tab, this is for documentation purpose only
+        # do not change the default value of 8, ever.
+        tabstop = 8;
+      };
+      extraConfig = builtins.readFile vim/vimrc;
+    };
+
     git = {
       enable = true;
       userName = "Pierre-Emmanuel Patry";
