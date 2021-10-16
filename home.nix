b58754f062a77b8881a6389ff3bb2c6dcae9e336
Pierre-Emmanuel Patry <pierre-emmanuel.patry@epita.fr>
2021-10-16 21:05:55 +0200

    refacto (bash): Migrate aliases to nix format.
    Bash aliases were in the extra init section as plain text. This worked
    but was not really nixy. Hence I changed it.

--- home.nix
+++ home.nix
@@ -89,9 +89,15 @@ in
       enable = true;
       historyControl = [ "ignorespace" ];
       historyIgnore = [ "exit" ];
+
+      shellAliases = {
+        fmt = ''
+          find \$(test -f format_marker && echo -newer format_marker) \( -name '*.cc' -o -name '*.hh' -o -name '*.[ch]' \) -exec clang-format -i -style=file \{\} \; && touch format_marker
+        '';
+      };
+
       initExtra = ''
         set -o vi
-        alias fmt="find \$(test -f format_marker && echo -newer format_marker) \( -name '*.cc' -o -name '*.hh' -o -name '*.[ch]' \) -exec clang-format -i -style=file \{\} \; && touch format_marker"
       '';
     };
 
