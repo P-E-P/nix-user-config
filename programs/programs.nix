afe8988612daeff8d55b5ef5202049cc9823c9d2
Pierre-Emmanuel Patry <pierre-emmanuel.patry@epita.fr>
2021-10-17 17:45:54 +0200

    refacto (programs): Indent file.
    Reindent file correctly.

--- programs/programs.nix
+++ programs/programs.nix
@@ -1,24 +1,24 @@
 { pkgs, ... }:
 
 {
-      home-manager.enable = true;
-      command-not-found.enable = true;
+  home-manager.enable = true;
+  command-not-found.enable = true;
 
-      vim = {
-        enable = true;
-        plugins = with pkgs.vimPlugins; [
-          vim-airline
-          rust-vim
-          vim-nix
-          vim-gutentags
-          jellybeans-vim
-        ];
-        settings = {
-          relativenumber = true;
-          number = true;
-          expandtab = true;
-          ignorecase = true;
-          shiftwidth = 4;
+  vim = {
+    enable = true;
+    plugins = with pkgs.vimPlugins; [
+      vim-airline
+      rust-vim
+      vim-nix
+      vim-gutentags
+      jellybeans-vim
+    ];
+    settings = {
+      relativenumber = true;
+      number = true;
+      expandtab = true;
+      ignorecase = true;
+      shiftwidth = 4;
           # The length of a tab, this is for documentation purpose only
           # do not change the default value of 8, ever.
           tabstop = 8;
