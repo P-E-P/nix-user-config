c06261ec55b2c6e94efb2bca666baa8be7be4e73
Pierre-Emmanuel Patry <pierre-emmanuel.patry@epita.fr>
2021-10-23 14:20:14 +0200

    feat (packageBundle): Add package bundle.
    Add a new module as a test for package bundles.

---
+++ modules/devJava.nix
@@ -0,0 +1,11 @@
+{ pkgs, ... }:
+
+let
+  unstable = import <nixos-unstable> { config.allowUnfree = true; };
+in
+  {
+    my.programs.packageBundles.devJava = with pkgs; [
+      unstable.adoptopenjdk-openj9-bin-16 maven
+    ];
+  }
+
---
+++ modules/programs.nix
@@ -0,0 +1,29 @@
+{ config, pkgs, lib, ...}:
+with lib;
+
+let
+  cfg = config.my.programs;
+in
+  {
+    options = {
+      my.programs = {
+        packageBundles = mkOption {
+          default = { };
+          type = with types; attrsOf (listOf package);
+          description = "Set of package bundles";
+        };
+
+        packages = mkOption {
+          default = [ ];
+          type = with types; listOf (oneOf [ package (listOf package) ]);
+          description = "Packages to install.";
+        };
+
+      };
+    };
+
+    imports = [
+      ./devJava.nix
+    ];
+  }
+
