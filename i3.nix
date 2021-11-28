6c092a8a1c9bc81482cad5da5120c6c95f006546
Pierre-Emmanuel Patry <pierre-emmanuel.patry@epita.fr>
2021-11-28 17:48:13 +0100

    refacto (i3): Add discord workspace assignation.
    Assign a workspace for discord.

--- i3.nix
+++ i3.nix
@@ -5,13 +5,29 @@ let
     url = https://wallpapers.etherealgames.com/wp-content/uploads/sites/6/2018/04/Doom-Wallpaper-003.jpg;
     sha256 = "1f207kh8c02xv4c03bylkxyw2ppcs3ns9dzc5hwfdqhrp53ij922";
   };
+  cfg = {
+    pkgs = pkgs;
+    mod = "Mod4";
+    workspaces = {
+    ws1 = "1";
+    ws2 = "2";
+    ws3 = "3";
+    ws4 = "4";
+    ws5 = "5";
+    ws6 = "6";
+    ws7 = "7";
+    ws8 = "8";
+    ws9 = "9";
+    ws10 = "10";
+    };
+  };
 in
   {
     enable = true;
     package = pkgs.i3;
 
     config = rec {
-      modifier = "Mod4";
+      modifier = cfg.mod;
       terminal = "alacritty";
 
       window.border = 1;
@@ -40,6 +56,15 @@ in
           ShiftLeft = "resize shrink width 2 px or 2 ppt";
           ShiftRight = "resize grow width 2 px or 2 ppt";
         };
+      };
+
+      window.commands = [
+        { command = "border pixel 0"; criteria = { class = "Firefox"; }; }
+        { command = "border pixel 0"; criteria = { class = "discord"; }; }
+      ];
+
+      assigns = {
+        "${cfg.workspaces.ws10}" = [ { class = "discord"; } ];
       };
 
       startup = [
